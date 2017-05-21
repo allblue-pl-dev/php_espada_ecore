980efa87b5eee18fa46f94532ce7228b011838d4
Magda <m.golec@allblue.pl>
2017-05-21 23:12:43 +0200

    lemon league to lemon bee graphics

--- LemonBee/layouts/index.php
+++ LemonBee/layouts/index.php
@@ -71,11 +71,11 @@
             <div class="magda-holder">
 	            <div class="col-lg-5 col-sm-6 col-sm-8 lb-login">
 	            	<div class="lb-login-content">
-	            		<div class="lb-login-logo col-sm-4 col-sm-4 ">
+	            		<div class="lb-login-logo col-sm-5 col-sm-5 ">
 	            			<img src="<?php echo E\Uri::File('LemonBee:images/logo.png'); ?>" />
                             <div class="mg-clear"></div>
                         </div>
-                        <div class="lb-login-form col-sm-8 col-sm-8">
+                        <div class="lb-login-form col-sm-7 col-sm-7">
                             <h3>
                                 <?php echo EC\HText::_('LemonBee:sys.texts_logInMessage'); ?>
                             </h3>
