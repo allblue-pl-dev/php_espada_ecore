809b6d0c54a100f0a1ed36924675f94f07caa1cb
SfTd <j.zolcik@allblue.pl>
2017-10-12 17:34:14 +0200

    `Mailer` uses `Mailer` in config (previously `Mail`).

--- Mailer/classes/HMailer.php
+++ Mailer/classes/HMailer.php
@@ -8,7 +8,7 @@ class HMailer extends E\Module
 
 	static public function NewMail($to_mail, $to_name)
 	{
-		$config = new EC\CConfig('Mail');
+		$config = new EC\CConfig('Mailer');
 
 		$mail = new CMail($config->from_Mail, $config->from_Name,
 				$to_mail, $to_name);
